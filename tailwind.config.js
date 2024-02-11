cfabf1a685716dd08891e0df883b85559fbb0bdf
Moritz Wright <moritzwright@MacBook-Pro-von-Moritz.local>
2024-02-11 21:48:18 +0100

    setup tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,18 @@
 module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+      mono: ["Space Mono", "monospace"],
+    },
+    screens: {
+      dashBoardBrakePoint: "550px",
+      sm: "425px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1280px",
+      "2xl": "1536px",
+    },
     extend: {},
   },
   plugins: [],
